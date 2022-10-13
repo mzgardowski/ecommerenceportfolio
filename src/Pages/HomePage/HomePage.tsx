2e94897d6df823458868ac184af76d49d7d25367
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-13 17:29:24 +0200

    Add Layout and Pages folders, Header.tsx, Footer.tsx, HomePage.tsx

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,14 @@
+import Footer from 'src/Layout/Footer/Footer';
+import Header from 'src/Layout/Header/Header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;

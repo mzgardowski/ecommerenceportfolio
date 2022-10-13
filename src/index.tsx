6c8f1529d937cdb9ad2f3569a99ea2c71a283f80
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-13 08:44:51 +0200

    Config husky, ts, pretiter, eslint, lint-staged

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root'),
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const rootElement = document.getElementById('root');
+const renderApp = (Component: () => JSX.Element) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>,
+    rootElement,
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+renderApp(App);
+if (module?.hot) {
+  module.hot.accept('./App', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const NextApp = require('./App').default;
+    renderApp(NextApp);
+  });
+}

765a07df95816d317bb0696e750cea586254b16e
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-17 17:24:19 +0200

    Set RTK Query, provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import HomePage from './Pages/HomePage/HomePage';
+import { useGetPokemonByNameQuery } from './api/request/fetchAll';
 
 function App() {
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur');
+  console.log('data', data, 'error', error, 'isLoading', isLoading);
   return (
     <div className='App'>
       <HomePage />
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const rootElement = document.getElementById('root');
 const renderApp = (Component: () => JSX.Element) => {
   ReactDOM.render(
-    <React.StrictMode>
-      <Component />
-    </React.StrictMode>,
+    <Provider store={store}>
+      <React.StrictMode>
+        <Component />
+      </React.StrictMode>
+    </Provider>,
     rootElement,
   );
 };

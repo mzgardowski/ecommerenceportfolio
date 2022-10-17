a8249a17b8cc4dca1e0b25bf4b101dbc3a62b5c7
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-17 18:03:08 +0200

    -

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,17 +1,12 @@
 import { Link } from 'react-router-dom';
+import './Header.module.css';
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <li>
-            <Link to='/'>HomePage</Link>
-          </li>
-          <li>
-            <Link to='/product'>Product</Link>
-          </li>
-        </ul>
+        <Link to='/'>HomePage</Link>
+        <Link to='/product'>Product</Link>
       </nav>
     </header>
   );

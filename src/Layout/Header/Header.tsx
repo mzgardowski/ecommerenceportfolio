838f9c10b18a697a1602b8cbb94e5af93d21865f
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-19 21:53:10 +0200

    Add AccountLogin.tsx. Edit Header.tsx

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,16 +1,17 @@
+import AccountLogin from 'src/Components/Layout/Header/AccountLogin';
 import Navigation from 'src/Components/Layout/Header/Navigation';
 import './Header.scss';
 
 const Header = () => {
   return (
-    <header className='row bg-light app-header'>
-      <Navigation />
-      <div className='col-6'>
-        <h4 className='text-center mt-3'>Clothes Shop</h4>
-      </div>
-      <div className='col-3'>
-        <i className='bi-alarm' color='black'></i>
-      </div>
+    <header>
+      <nav className='row bg-light app-header'>
+        <Navigation />
+        <div className='col-6'>
+          <h4 className='text-center mt-2'>Clothes Shop</h4>
+        </div>
+        <AccountLogin />
+      </nav>
     </header>
   );
 };

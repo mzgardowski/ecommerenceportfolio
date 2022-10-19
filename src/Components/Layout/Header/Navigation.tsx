af5291b439452970ff17e08a5fd899f502182ba4
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-19 21:31:08 +0200

    Add menu

--- src/Components/Layout/Header/Navigation.tsx
+++ src/Components/Layout/Header/Navigation.tsx
@@ -1,17 +1,53 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { List, ChevronLeft } from 'react-bootstrap-icons';
 
 const Navigation = () => {
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const onToggleMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
-    <nav className='col-md-3'>
-      <ul>
-        <li className='p-3'>
-          <Link to='/'>HomePage</Link>
-        </li>
-        <li>
-          <Link to='/product'>Product</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className='col-3 col-sm-3'>
+      {menu ? (
+        <ChevronLeft
+          cursor='pointer'
+          onClick={() => onToggleMenu()}
+          size={30}
+          className='m-2'
+        />
+      ) : (
+        <List
+          cursor='pointer'
+          onClick={() => onToggleMenu()}
+          size={30}
+          className='m-2'
+        />
+      )}
+      {menu && (
+        <div className='position-fixed bg-light col-12'>
+          <ul className='m-1 p-1 list-group'>
+            <li>
+              <Link onClick={() => onToggleMenu()} to='/'>
+                HomePage
+              </Link>
+            </li>
+            <li>
+              <Link onClick={() => onToggleMenu()} to='/product'>
+                Product
+              </Link>
+            </li>
+            <li>
+              <Link onClick={() => onToggleMenu()} to='/product'>
+                Product
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header className='row bg-light app-header'>
       <Navigation />
-      <div className='col-md-6'>
-        <h2 className='mt-3 text-center'>ecommerence DEMO</h2>
+      <div className='col-6'>
+        <h4 className='text-center mt-3'>Clothes Shop</h4>
       </div>
-      <div className='col-md-3'>
+      <div className='col-3'>
         <i className='bi-alarm' color='black'></i>
       </div>
     </header>

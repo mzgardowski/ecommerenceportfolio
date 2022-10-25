6db7d2c1ccdd48c7bb306e960681e0087eb4292f
Mateusz Zgardowski <mateusz.zgardowski@gmail.com>
2022-10-25 20:43:02 +0200

    Add tests config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-};
